352ea899b01e48cb48c67f6eaf29a44a013f5875
hi <he@y.com>
2021-04-11 09:53:55 -0400

    Fixed two issues

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,9 +2,11 @@
 #include <iomanip>
 using namespace std;
 
+// LF 1202.501  4/11/21
+
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f; // fixed order of operations
 }
 
 int main()
@@ -13,7 +15,7 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3; // changed cin from n2 to n3
 
 	float a = average(n1, n2, n3);
 
